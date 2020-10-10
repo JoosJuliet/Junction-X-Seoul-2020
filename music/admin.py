76e666bb18c4972f88a531c58e140cc556cf27f1
JoosJuliet <saven7788@gmail.com>
2020-10-11 02:12:01 +0900

    init db modeling

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Music, MusicLocation
+
+admin.site.register(User)
+admin.site.register(Music)
+admin.site.register(MusicLocation)
--- music/models.py
+++ music/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    zeppeto_hash_code = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.name} '
+
+
+class Music(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.name} '
+
+class MusicLocation(models.Model):
+    user = models.ForeignKey('User', on_delete=models.DO_NOTHING)
+    music = models.ForeignKey('Music', on_delete=models.DO_NOTHING)
+    longitude = models.IntegerField()
+    latitude = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.id} / {self.user} {self.music}'
