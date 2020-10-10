57803e73198d8d8f0503681d128d43355a72a0fa
JoosJuliet <saven7788@gmail.com>
2020-10-11 02:32:26 +0900

    estimate bound

--- music/views.py
+++ music/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 
-# Create your views here.
+import math
+def getLatLngBound(lat, lng):
+  # 5km 구간
+  lat_change = 5 / 111.2
+  lng_change = abs(math.cos(lat * (math.pi / 180)))
+  bounds = {
+    "lat_min": lat - lat_change,
+    "lng_min": lng - lng_change,
+    "lat_max": lat + lat_change,
+    "lng_max": lng + lng_change
+  }
+  return bounds
+
+
 def ping(request):
     return HttpResponse('pong')
 
@@ -13,7 +25,8 @@ def get_musics(request):
 #  POST /users/{zeppeto_hash_code}/music -> 계
 
 def post_user_memory(request, zeppeto_hash_code):
-
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
     return HttpResponse(zeppeto_hash_code)
 
 
