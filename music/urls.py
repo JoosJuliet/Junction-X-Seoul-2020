c98b973c39ac10714ac60c870d1c54b9ed566cc0
JoosJuliet <saven7788@gmail.com>
2020-10-11 02:27:11 +0900

    init url modeling

--- music/urls.py
+++ music/urls.py
@@ -4,7 +4,10 @@ from django.urls import path, include
 from music import views
 
 urlpatterns = [
-    path('', views.ping),
+    path(f'users/<slug:zeppeto_hash_code>/musics', views.get_user_musics),
+    path(f'musics', views.get_user_musics),
+    path(f'users/<slug:zeppeto_hash_code>/music', views.post_user_memory)
+
 ]
 
 
--- music/views.py
+++ music/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 # Create your views here.
 def ping(request):
     return HttpResponse('pong')
+
+# GET /musics
+def get_musics(request):
+    return HttpResponse('get_musics')
+
+
+#  POST /users/{zeppeto_hash_code}/music -> 계
+
+def post_user_memory(request, zeppeto_hash_code):
+
+    return HttpResponse(zeppeto_hash_code)
+
+
+
+
+def get_user_musics(request, zeppeto_hash_code):
+    return HttpResponse(zeppeto_hash_code)
+#GET /users/{zeppeto_hash_code}/musics
