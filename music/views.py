6a839f0c03f188c47e04799df334ece0a22d6980
JoosJuliet <saven7788@gmail.com>
2020-10-11 09:59:22 +0900

    get users memory

--- music/views.py
+++ music/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import HttpResponse
 
 import math
@@ -9,18 +10,10 @@ from rest_framework.viewsets import ModelViewSet
 from music.models import User, Music, MusicLocation
 from music.serializers import MusicLocadtionSerializer
 
+from rest_framework.decorators import action, api_view
 
-def getLatLngBound(lat, lng):
-  # 5km 구간
-  lat_change = 5 / 111.2
-  lng_change = abs(math.cos(lat * (math.pi / 180)))
-  bounds = {
-    "lat_min": lat - lat_change,
-    "lng_min": lng - lng_change,
-    "lat_max": lat + lat_change,
-    "lng_max": lng + lng_change
-  }
-  return bounds
+
+
 
 
 def ping(request):
@@ -36,31 +29,33 @@ from rest_framework.decorators import action, api_view
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
 
-    def destroy(self, request, *args, **kwargs):
-        return Response(data={'result': 'Success'})
 
-@api_view(['POST'])
+
+@api_view(['POST', 'GET'])
 def post_user_memory(request, zeppeto_hash_code):
-    data = request.data
-    user = User.objects.filter(zeppeto_hash_code=zeppeto_hash_code).first()
-    music = Music.objects.filter(name=data['music_name']).first()
-    music_location = MusicLocation.objects.create(user=user, music=music, longitude=data['longitude'], latitude=data['latitude'])
-    music_location.save()
+
     if request.method == 'POST':
-        return HttpResponse(data)
-    return HttpResponse()
 
+        # POST	/users/{zeppeto_hash_code}/music
+        # 	request	longitude
+        # 		latitude
+        # 		sing_name
+        user = User.objects.filter(zeppeto_hash_code=zeppeto_hash_code).first()
+        data = request.data
+        music = Music.objects.filter(name=data['music_name']).first()
+        if not music:
+            return HttpResponse('음악이 없습니다, 음악을 등록해주세요')
+        music_location = MusicLocation.objects.create(user=user, music=music, longitude=data['longitude'], latitude=data['latitude'])
+        music_location.save()
 
-# >>> r3 = Reporter(first_name='John', last_name='Smith', email='john@example.com')
-# >>> Article.objects.create(headline="This is a test", pub_date=date(2005, 7, 27), reporter=r3)
+        return HttpResponse('success')
 
-# POST	/users/{zeppeto_hash_code}/music
-# 	request	longitude
-# 		latitude
-# 		sing_name
-# 		zeppeto_hash_code
+    if request.method == 'GET':
+        # GET	/users/{zeppeto_hash_code}/music
+        # 	request	longitude
+        # 		latitude
+        # 		sing_name
+        # 이거는내노래list보내주기
+        user = MusicLocation.objects.select_related('user').filter(user__zeppeto_hash_code=zeppeto_hash_code).values_list('music__name', 'user__musiclocation__latitude','music__musiclocation__longitude')
 
-
-def get_user_musics(request, zeppeto_hash_code):
-    return HttpResponse(zeppeto_hash_code)
-#GET /users/{zeppeto_hash_code}/musics
+        return Response(user)
